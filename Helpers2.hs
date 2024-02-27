a3d1ec405777c37fc62efd80627fa0fd56464f5e
Jonathan Adithya <joadithya39@gmail.com>
2024-02-26 18:46:45 -0800

    add docs and test cases for helpers and utils

--- Helpers2.hs
+++ Helpers2.hs
@@ -8,6 +8,41 @@ import Utils (deleteListAtIndex, getUserAction, getUserInput, updateListAtIndex)
 
 --------------- Create group ---------------
 
+-- | createGroup function creates a new group in the state.
+-- 
+-- It prompts the user to enter the name of the group and checks if the group already exists.
+-- If the group already exists, it prompts the user to choose a different name.
+-- If the group doesn't exist, it inserts the group into the database, updates the state, and 
+-- returns the updated state.
+--
+-- Args:
+--   state: The current state.
+--
+-- Returns:
+--   The updated state after creating a new group.
+--
+-- Example (1):
+-- >>> createGroup (State [Group "Group 1"] [])
+-- Creating a new group
+-- -------------------
+-- Enter the name of the group: Group 1
+-- Group already exists. Please choose a different name.
+--
+-- Creating a new group
+-- -------------------
+-- Enter the name of the group: Group 2
+-- Group created successfully!
+--
+-- Example (2):
+-- >>> createGroup (State [] [])
+-- Creating a new group
+-- -------------------
+-- Enter the name of the group: Group 1
+-- Group created successfully!
+-- 
+-- 
+-- User Input Format:
+-- Desired name of the task group
 createGroup :: State -> IO State
 createGroup (State groups tasks) = do
   putStrLn "Creating a new group"
@@ -24,6 +59,48 @@ createGroup (State groups tasks) = do
 
 --------------- Create task ---------------
 
+-- | Creates a new task and updates the state accordingly.
+--
+-- If no groups exist, it asks the user to create a group first.
+-- Otherwise, it prompts the user to enter the title and description of the task,
+-- and then asks the user to select a group to add the task to.
+-- The task is then inserted into the database and added to the state.
+--
+-- Args:
+--   state: The current state.
+--
+-- Returns:
+--   The updated state after creating a new task.
+--
+-- Example (1):
+-- >>> createTask (State [] [])
+-- No groups found. Please create a group first.
+--
+-- Example (2):
+-- >>> createTask (State [Group "Group 1"] [])
+-- Creating a new task
+-- -------------------
+-- Enter the title of the task: Task 1
+-- Enter the description of the task:
+-- Task 1 description
+-- Which group do you want to add the task to?
+--   (1) Group 1
+-- Choose an option (1): 1
+-- Task created successfully!
+--
+-- Example (3):
+-- >>> createTask (State [Group "Group 1", Group "Group 2"] [])
+-- Creating a new task
+-- -------------------
+-- Enter the title of the task: Task 2
+-- Enter the description of the task:
+-- Task 2 description
+-- Which group do you want to add the task to?
+--   (1) Group 1
+--   (2) Group 2
+-- Choose an option (1-2): 2
+-- Task created successfully!
+--
 createTask :: State -> IO State
 createTask (State [] tasks) = do
   putStrLn "No groups found. Please create a group first.\n"
@@ -51,6 +128,46 @@ createTask (State groups tasks) = do
 
 --------------- View all tasks ---------------
 
+-- | Function to view all tasks in the state.
+--
+-- If there are no tasks, it prompts the user to go back ro exit.
+-- If there are tasks, it prompts the user to select a task to view.
+-- The user can also choose to go back or exit.
+--
+-- Args:
+--   state: The current state.
+--
+-- Returns:
+--   The updated state after viewing/editing tasks.
+--
+-- Example (1):
+-- >>> viewAllTasks (State [] [])
+-- No tasks found.
+--
+-- What do you want to do?
+--   (1) Back
+--   (2) Exit
+-- Choose an option (1-2): 1
+--
+-- Example (2):
+-- >>> viewAllTasks (State [Group "Group 1"] [Task "Task 1" "Task 1 description" (Group "Group 1")])
+-- Viewing all tasks
+-- ------------------
+--   (1) Task 1
+--   (2) Back
+--   (3) Exit
+-- Choose an option (1-3): 1
+--
+-- Example (3):
+-- >>> viewAllTasks (State [Group "Group 1", Group "Group 2"] [Task "Task 1" "Task 1 description" (Group "Group 1"), Task "Task 2" "Task 2 description" (Group "Group 2")])
+-- Viewing all tasks
+-- ------------------
+--   (1) Task 1
+--   (2) Task 2
+--   (3) Back
+--   (4) Exit
+-- Choose an option (1-4): 2
+--
 viewAllTasks :: State -> IO State
 viewAllTasks (State groups []) = do
   putStrLn "Viewing all tasks"
@@ -78,6 +195,57 @@ viewAllTasks state = do
 
 --------------- View task ---------------
 
+-- | This function allows the user to view a specific task in the state.
+--
+-- It takes the current state and the index of the task as input.
+-- It prints the task details and prompts the user for an action to perform on the task.
+-- The available actions are: change title, change description, change group, delete task, go back, and exit.
+-- Depending on the user's chosen action, it calls the corresponding action function and updates the state accordingly.
+-- Finally, it recursively calls itself with the updated state and the same task index.
+--
+-- Args:
+--   state: The current state.
+--   index: The index of the task in the task list.
+--
+-- Returns:
+--   The updated state after viewing/editing the task.
+--
+-- Example (1):
+-- >>> viewTask (State [Group "Group 1"] [Task "Task 1" "Task 1 description" (Group "Group 1")]) 1
+-- Viewing a task
+-- ----------------
+-- Group: Group 1
+-- Title: Task 1
+-- Description: 
+-- Task 1 description
+--
+-- What do you want to do?
+--   (1) Change title
+--   (2) Change description
+--   (3) Change group
+--   (4) Delete Task
+--   (5) Back
+--   (6) Exit
+-- Choose an option (1-6): 5
+--
+-- Example (2):
+-- >>> viewTask (State [Group "Group 1", Group "Group 2"] [Task "Task 1" "Task 1 description" (Group "Group 1"), Task "Task 2" "Task 2 description" (Group "Group 2")]) 2
+-- Viewing a task
+-- ----------------
+-- Group: Group 2
+-- Title: Task 2
+-- Description:
+-- Task 2 description
+-- 
+-- What do you want to do?
+--   (1) Change title
+--   (2) Change description
+--   (3) Change group
+--   (4) Delete Task
+--   (5) Back
+--   (6) Exit
+-- Choose an option (1-6): 6
+--
 viewTask :: State -> Int -> IO State
 viewTask state index = do
   putStrLn "Viewing a task"
@@ -104,6 +272,39 @@ viewTask state index = do
 
 --------------- Change title ---------------
 
+-- | Change the title of a task in the state.
+-- 
+-- If the task is found in the database, update its title and return the updated state.
+-- Otherwise, return the original state.
+--
+-- Args:
+--   state: The current state.
+--   task: The task to update.
+--   index: The index of the task in the task list.
+--
+-- Returns:
+--   The updated state if the task is found in the database, otherwise the original state.
+--
+-- Example (1):
+-- >>> changeTitle (State [Group "Group 1"] [Task "Task 1" "Task 1 description" (Group "Group 1")]) (Task "Task 1" "Task 1 description" (Group "Group 1")) 1
+-- Changing the title of the task
+-- ------------------------------
+-- Enter the new title of the task: Task 2
+-- Title changed successfully!
+
+-- Example (2):
+-- >>> changeTitle (State [Group "Group 1"] [Task "Task 1" "Task 1 description" (Group "Group 1")]) (Task "Task 1" "Task 1 description" (Group "Group 1")) 1
+-- Changing the title of the task
+-- ------------------------------
+-- Enter the new title of the task: Task 2
+-- Title changed successfully!
+--
+-- >>> changeTitle (State [Group "Group 1"] [Task "Task 1" "Task 1 description" (Group "Group 1")]) (Task "Task 1" "Task 1 description" (Group "Group 1")) 1
+-- Changing the title of the task
+-- ------------------------------
+-- Enter the new title of the task: Task 2
+-- Failed to find the task in the database.
+--
 changeTitle :: State -> Task -> Int -> IO State
 changeTitle state task index = do
   putStrLn "Changing the title of the task"
@@ -126,11 +327,43 @@ changeTitle state task index = do
 
 --------------- Change description ---------------
 
+-- | Change the description of a task in the state.
+--
+-- This function takes the current state, a task, and an index as input.
+-- It prompts the user to enter a new description for the task and updates
+-- the task's description in the database. If the task is found in the database,
+-- the description is updated and the state is returned with the updated task.
+-- If the task is not found, an error message is displayed and the original state is returned.
+--
+-- Args:
+--   state: The current state.
+--   task: The task to update.
+--   index: The index of the task in the task list.
+--
+-- Returns:
+--   The updated state if the task is found in the database, otherwise the original state.
+--
+-- Example (1):
+-- >>> changeDescription (State [Group "Group 1"] [Task "Task 1" "Task 1 description" (Group "Group 1")]) (Task "Task 1" "Task 1 description" (Group "Group 1")) 1
+-- Changing the description of the task
+-- ------------------------------
+-- Enter the new description of the task: 
+-- Task 1 new description
+-- Description changed successfully!
+
+-- Example (2):
+-- >>> changeDescription (State [Group "Group 1"] [Task "Task 1" "Task 1 description" (Group "Group 1")]) (Task "Task 2" "Task 1 description" (Group "Group 1")) 1
+-- Changing the description of the task
+-- ------------------------------
+-- Enter the new description of the task: 
+-- Task 1 new description
+-- Failed to find the task in the database.
+--
 changeDescription :: State -> Task -> Int -> IO State
 changeDescription state task index = do
   putStrLn "Changing the description of the task"
   putStrLn "------------------------------"
-  newDescription <- getUserInput "Enter the new description of the task: "
+  newDescription <- getUserInput "Enter the new description of the task:\n"
 
   taskIdMaybe <- fetchTaskIdByNameD dbPath (title task)
 
@@ -148,6 +381,38 @@ changeDescription state task index = do
 
 --------------- Change group ---------------
 
+-- | Change the group of a task in the state.
+-- The function prompts the user to select a new group for the task,
+-- updates the task's group in the database, and returns the updated state.
+--
+-- Args:
+--   state: The current state.
+--   task: The task to update.
+--   index: The index of the task in the task list.
+--
+-- Returns:
+--   The updated state after changing the group of the task.
+--
+-- Example (1):
+-- >>> changeGroup (State [Group "Group 1", Group "Group 2"] [Task "Task 1" "Task 1 description" (Group "Group 1")]) (Task "Task 1" "Task 1 description" (Group "Group 1")) 1
+-- Changing the group of the task
+-- ------------------------------
+-- Which group do you want to move the task to?
+--   (1) Group 1
+--   (2) Group 2
+-- Choose an option (1-2): 2
+-- Group changed successfully!
+--
+-- Example (2):
+-- >>> changeGroup (State [Group "Group 1", Group "Group 2"] [Task "Task 1" "Task 1 description" (Group "Group 1")]) (Task "Task 4" "Task 1 description" (Group "Group 1")) 1
+-- Changing the group of the task
+-- ------------------------------
+-- Which group do you want to move the task to?
+--   (1) Group 1
+--   (2) Group 2
+-- Choose an option (1-2): 2
+-- Failed to find the task in the database.
+--
 changeGroup :: State -> Task -> Int -> IO State
 changeGroup state task index = do
   putStrLn "Changing the group of the task"
@@ -179,6 +444,30 @@ changeGroup state task index = do
 
 --------------- Delete task ---------------
 
+-- | Deletes a task from the state based on the given index.
+-- 
+-- It also deletes the task from the database.
+-- 
+-- Args:
+--   state: The current state.
+--   index: The index of the task to delete. 
+--          Requires index to be a valid index in the task list in the state.
+--
+-- Returns:
+--  the updated state after deleting the task.
+
+-- Example (1)
+-- deleteTask (State [Group "Group 1"] [Task "Task 1" "Task 1 description" (Group "Group 1")]) 1
+-- Deleting a task
+-- ----------------
+-- Task deleted successfully!
+--
+-- Example (2) -- Assume that the state doesn't matches the database
+-- deleteTask (State [Group "Group 1", Group "Group 2"] [Task "Task 1" "Task 1 description" (Group "Group 1"), Task "Task 2" "Task 2 description" (Group "Group 2")]) 1
+-- Deleting a task
+-- ----------------
+-- Failed to find the task in the database.
+--
 deleteTask :: State -> Int -> IO State
 deleteTask state index = do
   putStrLn "Deleting a task"
@@ -188,14 +477,43 @@ deleteTask state index = do
   taskId <- fetchTaskIdByNameD dbPath (title taskToDelete)
 
   case taskId :: Maybe Int of
-    Just taskId -> deleteTaskD dbPath taskId
-
-  let newTasks = deleteListAtIndex (tasks state) index
-  putStrLn "Task deleted successfully!\n"
-  return $ State (groups state) newTasks
+    Just taskId -> do
+      deleteTaskD dbPath taskId
+      let newTasks = deleteListAtIndex (tasks state) index
+      putStrLn "Task deleted successfully!\n"
+      return $ State (groups state) newTasks
+    Nothing -> do 
+      putStrLn "Failed to find the task in the database."
+      return state
 
 --------------- Delete group ---------------
 
+-- | Deletes a group from the state based on the given index.
+--
+-- It also deletes all tasks associated with the group.
+-- 
+-- Args:
+--   state: The current state.
+--   index: The index of the group to delete.
+--          Requires index to be a valid index in the group list in the state.
+--
+-- Returns:
+--  the updated state after deleting the group and its tasks.
+--
+-- Example (1)
+-- deleteGroup (State [Group "Group 1"] [Task "Task 1" "Task 1 description" (Group "Group 1")]) 1
+-- Deleting a group
+-- ----------------
+-- Group and its tasks deleted successfully!
+--
+-- Returns: State [] []
+--
+-- Example (2) -- Assume that the state doesn't matches the database
+-- deleteGroup (State [Group "Group 1", Group "Group 2"] [Task "Task 1" "Task 1 description" (Group "Group 1"), Task "Task 2" "Task 2 description" (Group "Group 2")]) 1
+-- Deleting a group
+-- ----------------
+-- Failed to find the group in the database.
+--
 deleteGroup :: State -> Int -> IO State
 deleteGroup state index = do
   putStrLn "Deleting a group"
@@ -206,13 +524,50 @@ deleteGroup state index = do
   case groupIdMaybe of
     Just groupId -> do
       deleteGroupD dbPath groupId
-
       let newGroups = deleteListAtIndex (groups state) index
       putStrLn "Group and its tasks deleted successfully!\n"
       return $ State newGroups (filter (\task -> group task /= Group groupName) (tasks state))
+    Nothing -> do
+      putStrLn "Failed to find the group in the database."
+      return state
 
 --------------- View all tasks by group ---------------
 
+-- | Function to view all groups and their tasks in the state.
+--
+-- If no groups are found, it prompts the user to create a group first.
+-- Otherwise, it prompts the user to select a group and then displays all tasks in that group.
+-- Returns the updated state after viewing all groups.
+--
+-- Args:
+--   state: The current state.
+--
+-- Returns:
+--   The updated state after viewing all groups (probably some tasks are updated).
+--
+-- Example (1):
+-- >>> viewAllGroups (State [] [])
+-- No groups found. Please create a group first.
+--
+-- Example (2):
+-- >>> viewAllGroups (State [Group "Group 1"] [Task "Task 1" "Task 1 description" (Group "Group 1")])
+-- Viewing all tasks by group
+-- ---------------------------
+-- Which group do you want to view?
+--   (1) Group 1
+-- Choose an option (1): 1
+-- ** Calls viewAllTasksByGroup **
+--
+-- Example (3):
+-- >>> viewAllGroups (State [Group "Group 1", Group "Group 2"] [Task "Task 1" "Task 1 description" (Group "Group 1"), Task "Task 2" "Task 2 description" (Group "Group 2")])
+-- Viewing all tasks by group
+-- ---------------------------
+-- Which group do you want to view?
+--   (1) Group 1
+--   (2) Group 2
+-- Choose an option (1-2): 2
+-- ** Calls viewAllTasksByGroup **
+--
 viewAllGroups :: State -> IO State
 viewAllGroups (State [] tasks) = do
   putStrLn "No groups found. Please create a group first.\n"
@@ -227,13 +582,59 @@ viewAllGroups state = do
   let group = groups state !! (groupIndex - 1)
   viewAllTasksByGroup state group
 
+-- | viewAllTasksByGroup is a function that takes a State and a Group as input and displays all tasks in the selected group.
+--
+-- If there are no tasks in the group, it prompts the user for further action.
+-- The function returns an updated State if the user chooses to go back, or exits the program.
+--
+-- Args:
+--   state: The current state.
+--   selectedGroup: The group to view tasks for.
+--
+-- Returns:
+--   The updated state after viewing all tasks in the selected group.
+--
+-- Example (1):
+-- >>> viewAllTasksByGroup (State [Group "Group 1"] [Task "Task 1" "Task 1 description" (Group "Group 1")]) (Group "Group 1")
+-- Viewing all tasks in Group 1 group
+-- ---------------------------
+--   (1) Task 1
+--   (2) Back
+--   (3) Exit
+-- Choose an option (1-3): 1
+-- ** Calls viewTask **
+--
+-- Example (2):
+-- >>> viewAllTasksByGroup (State [Group "Group 1"] []) (Group "Group 1")
+-- Viewing all tasks in Group 1 group
+-- ---------------------------
+-- No tasks found.
+--
+-- What do you want to do?
+--   (1) Back
+--   (2) Exit
+-- Choose an option (1-2): 1
+--
+-- Example (3):
+-- >>> viewAllTasksByGroup (State [Group "Group 1", Group "Group 2"] [Task "Task 1" "Task 1 description" (Group "Group 1"), Task "Task 2" "Task 2 description" (Group "Group 2")]) (Group "Group 2")
+-- Viewing all tasks in Group 2 group
+-- ---------------------------
+--   (1) Task 2
+--   (2) Back
+--   (3) Exit
+-- Choose an option (1-3): 2
+--
 viewAllTasksByGroup :: State -> Group -> IO State
 viewAllTasksByGroup state selectedGroup = do
+  -- Displaying the selected group
   putStrLn $ "Viewing all tasks in " ++ show selectedGroup ++ " group"
   putStrLn "---------------------------"
+  
+  -- Indexing the tasks and filtering based on the selected group
   let indexedTasks = zip [1 ..] (tasks state)
   let filteredTasks = filter (\(index, task) -> group task == selectedGroup) indexedTasks
 
+  -- Handling the case when no tasks are found
   if null filteredTasks
     then do
       putStrLn "No tasks found.\n"
@@ -243,6 +644,7 @@ viewAllTasksByGroup state selectedGroup = do
       case action of
         1 -> return state
         2 -> exitSuccess
+    -- Handling the case when tasks are found
     else do
       let prompt = "Which task do you want to view?"
       let actions = map (\(_, task) -> title task) filteredTasks ++ ["Back", "Exit"]
--- Utils.hs
+++ Utils.hs
@@ -5,12 +5,88 @@ import Types
 
 import System.Console.ANSI (clearScreen, setCursorPosition)
 
+-- | Updates the element at the specified index in a list.
+--
+-- The function takes a list, an index, and a new element. It returns a new list
+-- where the element at the specified index is replaced with the new element.
+-- Requires the index to be a valid index in the list.
+--
+-- Args:
+--   list: The list to update.
+--   index: The index of the element to update.
+--   newElement: The new element to replace the old element at the specified index.
+--
+-- Returns:
+--   A new list with the element at the specified index replaced with the new element.
+--
+-- Examples:
+--
+-- >>> updateListAtIndex [1, 2, 3] 1 4
+-- [4,2,3]
+--
+-- >>> updateListAtIndex [1, 2, 3] 2 4
+-- [1,4,3]
+--
+-- >>> updateListAtIndex [1, 2, 3] 3 4
+-- [1,2,4]
+--
 updateListAtIndex :: [a] -> Int -> a -> [a]
 updateListAtIndex list index newElement = take (index - 1) list ++ [newElement] ++ drop index list
 
+-- | Deletes an element from a list at the specified index.
+--
+-- Returns a new list with the element removed.
+-- Requires the index to be a valid index in the list.
+--
+-- Examples:
+--
+-- >>> deleteListAtIndex [1,2,3,4,5] 2
+-- [1,3,4,5]
+--
+-- >>> deleteListAtIndex [1,2,3,4,5] 5
+-- [1,2,3,4]
+--
+-- >>> deleteListAtIndex [1,2,3,4,5] 1
+-- [2,3,4,5]
+--
 deleteListAtIndex :: [a] -> Int -> [a]
 deleteListAtIndex list index = take (index - 1) list ++ drop index list
 
+-- | Prompts the user with a message and a list of actions, and returns the selected action index.
+--
+-- If the user enters an invalid input, it prompts again until a valid input is provided.
+--
+-- Args:
+--   prompt: The message to display to the user.
+--   actions: The list of actions to display to the user.
+--
+-- Returns:
+--   The index of the selected action.
+--
+-- Examples:
+--
+-- >>> getUserAction "Choose an action:" ["Action 1", "Action 2", "Action 3"]
+-- Choose an action:
+--   (1) Action 1
+--   (2) Action 2
+--   (3) Action 3
+-- Choose an option (1-3): 2
+-- Returns 2
+--
+-- >>> getUserAction "Choose an action:" ["Action 1", "Action 2", "Action 3"]
+-- Choose an action:
+--   (1) Action 1
+--   (2) Action 2
+--   (3) Action 3
+-- Choose an option (1-3): 4
+-- Invalid action. Please choose a valid action.
+-- Choose an action:
+--   (1) Action 1
+--   (2) Action 2
+--   (3) Action 3
+-- Choose an option (1-3): 3
+-- Returns 3
+--
 getUserAction :: String -> [String] -> IO Int
 getUserAction prompt actions = do
   putStrLn prompt >> printActions 1 actions
@@ -26,26 +102,102 @@ getUserAction prompt actions = do
       invalidAction
       getUserAction prompt actions
  where
+  -- | Prints the list of actions with their corresponding indices.
   printActions :: Int -> [String] -> IO ()
   printActions _ [] = return ()
   printActions index (action : rest) = do
     putStrLn $ "  (" ++ show index ++ ") " ++ action
     printActions (index + 1) rest
+  
+  -- | Returns the prompt for selecting an option based on the number of actions.
   printOptions :: Int -> String
   printOptions 1 = "Choose an option (1): "
   printOptions n = "Choose an option (1-" ++ show n ++ "): "
 
+-- | Prompts the user with a message and retrieves their input from the command line.
+--
+-- The input is then processed to remove any trailing newline characters.
+--
+-- Args:
+--   prompt: The message to display to the user.
+--
+-- Returns:
+--   The user's input.
+--
+-- Examples:
+--
+-- >>> getUserInput "Enter a number: "
+-- Enter a number: 5
+-- Returns "5"
+--
+-- >>> getUserInput "Enter a number: \n"
+-- Enter a number: 
+-- 5
+-- Returns "5"
+--
 getUserInput :: String -> IO String
 getUserInput prompt = putStr prompt >> getLine >>= fixdel
 
+-- | Prints an error message indicating that an invalid action was chosen.
+--
+-- Examples:
+--
+-- >>> invalidAction
+-- Invalid action. Please choose a valid action.
+--
 invalidAction :: IO ()
 invalidAction = putStrLn "Invalid action. Please choose a valid action.\n"
 
+
+-- | This function takes a string as input and removes all occurrences of the backspace character ('\DEL') from the string.
+--
+-- It recursively calls itself until there are no more backspace characters in the string.
+-- The resulting string is returned wrapped in the IO monad.
+--
+-- Args:
+--   string: The string to remove backspace characters from.
+--
+-- Returns:
+--   The string with all backspace characters removed.
+--
+-- Examples:
+--
+-- >>> fixdel "abc\DEL\DEL\DELdef"
+-- "def"
+--
+-- >>> fixdel "abc\DEL"
+-- "ab"
+--
+-- >>> fixdel "abc\DEL\DEL\DEL"
+-- ""
+--
 fixdel :: [Char] -> IO [Char]
 fixdel string
   | '\DEL' `elem` string = fixdel (remdel string)
   | otherwise = return string
 
+-- | Removes the backspace character ('\DEL') and the character preceding it from a string.
+--
+-- If the string contains multiple backspace characters, it removes each pair of backspace and preceding character.
+-- Returns the modified string.
+--
+-- Args:
+--   string: The string to remove backspace characters from.
+--
+-- Returns:
+--   The string with all backspace characters removed.
+--
+-- Examples:
+-- 
+-- >>> remdel "abc\DEL\DEL\DELdef"
+-- "def"
+--
+-- >>> remdel "abc\DEL"
+-- "ab"
+--
+-- >>> remdel "abc\DEL\DEL\DEL"
+-- ""
+--
 remdel :: [Char] -> [Char]
 remdel ('\DEL' : rest) = rest
 remdel (a : '\DEL' : rest) = rest
